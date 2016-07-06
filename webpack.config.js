a7307d76acab17eaac909c6c9a4307c7a0b3f147
Alek Merani <alekmerani@aleks-mbp.attlocal.net>
2016-07-06 18:29:31 -0500

    Update webpack config to start at new app loc

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var webpack = require('webpack');
 module.exports = {
   entry: [
     'babel-polyfill',
-    './src/app',
+    './src/app/app',
     'webpack-dev-server/client?http://localhost:8080'
   ],
   output: {
@@ -22,7 +22,7 @@ module.exports = {
         include: path.join(__dirname, 'src'),
         loader: 'babel-loader',
         query: {
-          presets: ["es2015"],  
+          presets: ["es2015"],
         }
       }
     ]
